79260f4c08ba0a02a09b84fe54d159f5d100b078
Hieu Nguyen <hieund26@fpt.com.vn>
2019-04-16 16:12:34 +0700

    add auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
         });
     }
 
